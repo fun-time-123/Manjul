c5eb324cfe4cba50524a8af43611343547e3e30d
fun-time-123 <manjulsonkar09@gmail.com>
2025-06-22 10:15:15 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 async function analyzeStock() {
     const stock = document.getElementById("stockInput").value;
-    const response = await fetch(`https://manjul-1.onrender.com/analyze?stock=${stock}`);
+    const response = await fetch(`https://kuhu-intraday-backend.onrender.com/analyze?stock=${stock}`);
     const data = await response.json();
     document.getElementById("output").textContent = JSON.stringify(data, null, 2);
 }
